283225e9d1122fe03cb7a2dd08e6ef8722f5afc3
Existency <24860696+Existency@users.noreply.github.com>
2022-11-15 19:25:31 +0000

    fix benchmarks

--- KMeans/benchmarks/bench.sh
+++ KMeans/benchmarks/bench.sh
@@ -1,34 +1,26 @@
 #!/bin/bash
 
-PREFIX="perf stat -r 20 ./bin/k_means "
-
-# wait function
-wait_jobs(lc) {
-    while [ $(squeue -u $USER | wc -l) -gt $lc ]; do sleep 2; done
-}
-
 for i in 4 8 16 32
 do
     for j in 2 4 8 16
     do
-        # SeARCH limits the number of parallel jobs to 2
-        wait_jobs(2)
-        $PREFIX 10000000 $i $j
+        while [ $(squeue -u $USER | wc -l) -gt 2 ]; do sleep 2; done
+        perf stat -r 20 ./bin/k_means $i $j
     done
 done
 
 # run the rest of the benchmarks
-wait_jobs(2)
-$PREFIX 10000000 4
-wait_jobs(2)
-$PREFIX 10000000 8
-wait_jobs(2)
-$PREFIX 10000000 16
-wait_jobs(2)
-$PREFIX 10000000 32
+while [ $(squeue -u $USER | wc -l) -gt 2 ]; do sleep 2; done
+perf stat -r 20 ./bin/k_means 10000000 4
+while [ $(squeue -u $USER | wc -l) -gt 2 ]; do sleep 2; done
+perf stat -r 20 ./bin/k_means 10000000 8
+while [ $(squeue -u $USER | wc -l) -gt 2 ]; do sleep 2; done
+perf stat -r 20 ./bin/k_means 10000000 16
+while [ $(squeue -u $USER | wc -l) -gt 2 ]; do sleep 2; done
+perf stat -r 20 ./bin/k_means 10000000 32
 
 # wait for all jobs to finish
-wait_jobs(1)
+while [ $(squeue -u $USER | wc -l) -gt 2 ]; do sleep 2; done
 
 # prepare the output folder
 mkdir -p output
