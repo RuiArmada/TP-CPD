cb22628069b10ba59dbb3ddb7ca20e3f2100a584
Existency <24860696+Existency@users.noreply.github.com>
2022-11-16 17:48:38 +0000

    remove unused function in gen_samples, fix point.h unnecessary code

--- KMeans/include/common/point.h
+++ KMeans/include/common/point.h
@@ -1,8 +1,6 @@
 #ifndef POINT_H
 #define POINT_H
 
-#include <immintrin.h>
-
 typedef struct point {
     float x;
     float y;
--- KMeans/src/gen_samples.c
+++ KMeans/src/gen_samples.c
@@ -1,8 +1,7 @@
 #include "../include/gen_samples.h"
 #include "../include/common/point.h"
 
-#include <omp.h>
-#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 /**
@@ -18,26 +17,6 @@ void gen_sample_seq(point* samples, point* clusters, uint32_t cluster_count, uin
 
     // printf("Generating %d samples and %d clusters\n", sample_count, cluster_count);
 
-    for (uint_fast32_t i = 0; i < sample_count; i += 1) {
-        samples[i].x = (float)rand() / RAND_MAX;
-        samples[i].y = (float)rand() / RAND_MAX;
-    }
-
-    memcpy(clusters, samples, cluster_count * sizeof(point));
-}
-
-/**
- * @brief Generates a set of random points, in parallel, using pragma omp parallel for (OpenMP).
- *
- * @param samples
- * @param clusters
- * @param cluster_count
- * @param sample_count
- */
-void gen_samples_par(point* samples, point* clusters, uint32_t cluster_count, uint32_t sample_count) {
-    srand(10);
-
-    #pragma omp parallel for
     for (uint_fast32_t i = 0; i < sample_count; i += 1) {
         samples[i].x = (float)rand() / RAND_MAX;
         samples[i].y = (float)rand() / RAND_MAX;
